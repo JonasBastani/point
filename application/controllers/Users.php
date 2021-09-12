56425b02c1d151105dda7305a2abb2cd49574dd2
Jonas_Bastani <jonasbastani1999@gmail.com>
2021-09-12 14:15:58 -0300

    initial commit

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -19,41 +19,40 @@ class Users extends CI_Controller {
 
         $this->api->allowedMethods(['POST']);
 
-        $data = json_decode(file_get_contents("php://input"), true);
+        $data = json_decode(file_get_contents("php://input"), true); // recebe valores passados pelo usuário em data
 
-        if(!empty($data)){
-            if(!empty($data['user_password'])){
-                $data['user_password'] = md5($data['user_password']);
+        if(!empty($data)){ // caso data não esteja vazio segue para o insert ou update
+            if(!empty($data['user_password'])){ // se tiver sido passado um password(senha)
+                $data['user_password'] = md5($data['user_password']); //criptografa password(senha)
             }
-            if(!empty($data['user_id'])){
-                $user_id = $data["user_id"];
-				unset($data["user_id"]);
-
-                if($this->users_model->is_duplicated('user_cpf', $data['user_cpf'], $user_id)){
-                    $this->api->response(405, array('status' => false, 'message' => "CPF de outra conta!"));
-                }else if($this->users_model->is_duplicated('user_email', $data['user_email'], $user_id)){
-                    $this->api->response(405, array('status' => false, 'message' => "E-mail de outra conta!"));
-                }else{
-                    $data['updated_at'] = date("Y-m-d H:i:s");
-                    $this->users_my_model->update($data, $user_id);
-                    $this->api->response(200, array('status' => true, 'data'=> $data, 'message' => "Dados alterados com sucesso!"));
+            if(!empty($data['user_id'])){ // se tiver passado um user_id segue para update
+                $user_id = $data["user_id"];// iguala uma variavel user_id ao valor user_id que está em data[]
+				unset($data["user_id"]); // retira user_id de data[]
+                if($this->users_model->is_duplicated('user_cpf', $data['user_cpf'], $user_id)){ // verifica se o cpf passado já existe em outros usuarios
+                    $this->api->response(405, array('status' => false, 'message' => "CPF de outra conta!")); // se existir, retorna mensagem de erro
+                }else if($this->users_model->is_duplicated('user_email', $data['user_email'], $user_id)){ // verifica se o email passado já existe em outros usuarios
+                    $this->api->response(405, array('status' => false, 'message' => "E-mail de outra conta!")); // se existir, retorna mensagem de erro
+                }else{ // se não existe dados duplicados segue para o update
+                    $data['updated_at'] = date("Y-m-d H:i:s"); //adiciona horario do update em data
+                    $this->users_my_model->update($data, $user_id); // faz update
+                    $this->api->response(200, array('status' => true, 'data'=> $data, 'message' => "Dados alterados com sucesso!")); // retorna sucesso
                 }
 
-            }else{
-                if($this->users_model->is_duplicated('user_cpf', $data['user_cpf'])){
-                    $this->api->response(405, array('status' => false, 'message' => "Este CPF já possui cadastro!"));
-                }else if($this->users_model->is_duplicated('user_email', $data['user_email'])){
-                    $this->api->response(405, array('status' => false, 'message' => "Este E-mail já possui cadastro!"));
+            }else{ // se nao estiver passado um user_id segue para o insert
+                if($this->users_model->is_duplicated('user_cpf', $data['user_cpf'])){ // verifica se o cpf ja existe em algum usuário
+                    $this->api->response(405, array('status' => false, 'message' => "Este CPF já possui cadastro!")); // se existir, retorna mensagem de erro
+                }else if($this->users_model->is_duplicated('user_email', $data['user_email'])){ // verifica se email já existe em algum usuário
+                    $this->api->response(405, array('status' => false, 'message' => "Este E-mail já possui cadastro!")); // se existir, retorna mensagem de erro
                 }
-                else{
-                    $data['created_at'] = date("Y-m-d H:i:s");
-                    $data['active'] = 1;
-                    $this->users_my_model->insert($data);
-                    $this->api->response(200, array('status' => true, 'data'=> $data, 'message' => "Usuário cadastrado com sucesso!"));
+                else{ // se não existe dados duplicados segue para o insert
+                    $data['created_at'] = date("Y-m-d H:i:s"); // adiciona horario do insert em data
+                    $data['active'] = 1; // este atributo é 0 ou 1, 1 pra um usuario ativo e 0 para inativo, todo usuário inicia ativo
+                    $this->users_my_model->insert($data); // faz o insert
+                    $this->api->response(200, array('status' => true, 'data'=> $data, 'message' => "Usuário cadastrado com sucesso!")); //retorna sucesso
                 }
             }
-        }else{
-            $this->api->response(400, array('status' => false, 'message' => "Dados insuficientes!"));
+        }else{ // caso data esteja vazio
+            $this->api->response(400, array('status' => false, 'message' => "Dados insuficientes!")); // retorna mensagem de erro
         }
     }
 }
